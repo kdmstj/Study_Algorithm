84d8983611c09402ad697234c351c51e06fabc8c
kdmstj <rkddmstj429@gmail.com>
2022-01-17 22:41:16 +0900

    01/17

---
+++ src/입출력문제/ANS1924.java
@@ -0,0 +1,24 @@
+package 입출력문제;
+
+import java.util.Scanner;
+
+public class ANS1924 {
+
+    public static void Main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        for(int i = 1 ; i < 13; i ++){
+            if(i%2 != 0){
+                for(int k = 1; i< 32 ; k++){
+
+                }
+            }
+        }
+
+
+    }
+}
