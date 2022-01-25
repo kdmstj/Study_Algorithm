279a889da685332e71fe7fc6e2336bf2418a7220
kdmstj <rkddmstj429@gmail.com>
2022-01-25 23:57:46 +0900

    01/25

--- src/DP/ANS1912.java
+++ src/DP/ANS1912.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ANS1912 {
 
     static int N;
-    static long[] dp, A;
+    static int[] dp, A;
 
     public static void main(String[] args){
 
@@ -15,27 +15,19 @@ public class ANS1912 {
         N = sc.nextInt();
 
         //수열 A, 배열 dp 선언 및 초기화
-        A = new long[N];
-        dp = new long[N];
+        A = new int[N];
+        dp = new int[N];
         for(int i = 0 ; i < N ;i++){
             A[i] = sc.nextInt();
             dp[i] = 0;
         }
         dp[0] = A[0];
+        int max = A[0];
 
         for(int i = 1 ; i < N ; i++){
-            if(dp[i-1] + A[i] > A[i]){
-                dp[i] = dp[i-1] + A[i];
-            }
-            else{
-                dp[i] = A[i];
-            }
-        }
+            dp[i] = Math.max(dp[i-1] + A[i], A[i]);
+            max = Math.max(max,dp[i]);
 
-
-        long max = -1;
-        for(int i = 0 ; i < N; i++){
-            max = max < dp[i]  ? dp[i] : max;
         }
 
         System.out.println(max);
