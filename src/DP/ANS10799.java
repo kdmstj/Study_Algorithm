10b0ffe3acef5058e397f3e9e8341ef16619e6b4
kdmstj <rkddmstj429@gmail.com>
2022-02-07 16:02:29 +0900

    02/07

---
+++ src/DP/ANS10799.java
@@ -0,0 +1,12 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ANS10799 {
+
+    public static void main(String[] args){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
